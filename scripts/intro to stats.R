37505b52e379dfefacead6b920e82d48315af1f0
Gemma <saa21wpu@uea.ac.uk>
2023-02-17 16:53:21 +0000

    completed chapter 13

--- scripts/intro to stats.R
+++ scripts/intro to stats.R
@@ -137,3 +137,122 @@ upperCI <- 2.62+(2*1.22)
 lowerCI
 upperCI
 
+# NEW PACKAGES ____________________________________________ ----
+
+library(tidyverse)
+library(GGally)
+library(emmeans)
+library(performance)
+
+# LINEAR MODEL ANALYSIS FOR COMPARING GROUPS ____________________________ ----
+
+lsmodel0 <- lm(formula = height ~ 1, data = darwin)
+
+# MODEL SUMMARY TIDYVERSE________________________ ----
+
+broom::tidy(lsmodel0)
+mean(darwin$height)
+
+# MODEL SUMMARY BASE R ___________________________________ ----
+
+summary(lsmodel0)
+mean(darwin$height)
+
+# COMPARE MEANS ___________________________________ ----
+
+lsmodel1 <- lm(height ~ type, data=darwin)
+
+# note that the following is identical
+
+# lsmodel1 <- lm(height ~ 1 + type, data=darwin)
+
+broom::tidy(lsmodel1)
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height))
+
+summary(lsmodel1)
+
+darwin %>% 
+  ggplot(aes(x=type, 
+             y=height,
+             colour=type))+
+  geom_jitter(alpha=0.5,
+              width=0.1)+
+  stat_summary(fun=mean,
+               size=1.2)+
+  theme_bw()
+
+# CONFIDENCE INTERVALS 
+
+confint(lsmodel1)
+
+GGally::ggcoef_model(lsmodel1,
+                     show_p_values=FALSE, 
+                     conf.level=0.95)
+
+broom::tidy(lsmodel1, conf.int=T, conf.level=0.99)
+
+# MEAN AND STANDARD ERROR _____________________________ ----
+
+darwin %>% 
+  mutate(type=factor(type)) %>% 
+  mutate(type=fct_relevel(type, c("Self", "Cross"))) %>% 
+  lm(height~type, data=.) %>% 
+  broom::tidy()
+
+# EMMEANS ____________________________________________ ----
+
+means <- emmeans::emmeans(lsmodel1, specs = ~ type)
+
+means
+
+means %>% 
+  as_tibble() %>% 
+  ggplot(aes(x=type, 
+             y=emmean))+
+  geom_pointrange(aes(
+    ymin=lower.CL, 
+    ymax=upper.CL))
+
+# ASSUMPTION CHECKING BASE R ___________________________________________ ----
+
+performance::check_model(lsmodel1)
+
+performance::check_model(lsmodel1, check=c("normality","qq"))
+
+# ASSUMPTION CHECKING TIDYVERSE __________________________________ ----
+
+plot(lsmodel1)
+
+# NORMAL DISTRIBUTION _____________________________________________ ----
+
+performance::check_model(lsmodel1, check=c("normality","qq"))
+
+plot(lsmodel1, which=c(2,2))
+
+# EQUAL VARIANCE __________________________________________________________ ----
+
+performance::check_model(lsmodel1, check="homogeneity")
+
+plot(lsmodel1, which=c(1,3))
+
+# OUTLIERS _______________________________________ ----
+
+performance::check_model(lsmodel1, check="outliers")
+
+plot(lsmodel1, which=c(4,4))
+
+# SUMMARY _____________________________________________ ----
+
+darwin %>% 
+  ggplot(aes(x=type, 
+             y=height))+
+  geom_jitter(width=0.1, 
+              pch=21, 
+              aes(fill=type))+
+  theme_classic()+
+  geom_segment(aes(x=1, xend=2, y=20.192, yend=20.192-2.617), linetype="dashed")+
+  stat_summary(fun.y=mean, geom="crossbar", width=0.2)
+
