5623dce1237f3baf50c1b8ac52ad71cef4ab5421
Gemma <saa21wpu@uea.ac.uk>
2023-02-24 12:55:29 +0000

    work for chapter 14

--- scripts/intro to stats.R
+++ scripts/intro to stats.R
@@ -256,3 +256,22 @@ darwin %>%
   geom_segment(aes(x=1, xend=2, y=20.192, yend=20.192-2.617), linetype="dashed")+
   stat_summary(fun.y=mean, geom="crossbar", width=0.2)
 
+# STUDENT T TEST BASE R ______________________________________ ----
+
+x <- seq(-4, 4, length=100)
+hx <- dnorm(x)
+
+degf <- c(1, 3, 8, 30)
+colors <- c("red", "blue", "darkgreen", "gold", "black")
+labels <- c("df=1", "df=3", "df=8", "df=30", "normal")
+
+plot(x, hx, type="l", lty=2, xlab="x value",
+     ylab="Density", main="Comparison of t Distributions")
+
+for (i in 1:4){
+  lines(x, dt(x,degf[i]), lwd=2, col=colors[i])
+}
+
+legend("topright", inset=.05, title="Distributions",
+       labels, lwd=2, lty=c(1, 1, 1, 1, 2), col=colors)
+
